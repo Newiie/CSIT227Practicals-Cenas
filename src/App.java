cea274bfb958b532b3e356ad1356ea9f3b833808
Jethro Cenas <jethrocenas@gmail.com>
2022-12-21 12:00:41 +0800

    MileStone H

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class App {
+public class App extends JFrame {
     private JPanel pnlMain;
     private JRadioButton rbCustomer;
     private JRadioButton rbClerk;
@@ -20,19 +23,181 @@ public class App {
     private JButton btnSavePerson;
     private JButton btnLoadPerson;
     private JButton btnReward;
-
+    private List<JTextField> fields;
     private List<Person> persons;
-
+    private List<JRadioButton> info;
     public App() {
         persons = new ArrayList<>();
         // TODO add implementations for all milestones here
+        info = new ArrayList<>();
+        info.add(rbClerk);
+        info.add(rbCustomer);
+        info.add(rbManager);
+
+        fields = new ArrayList<>();
+        fields.add(tfAge);
+        fields.add(tfName);
+        fields.add(tfLoad);
+        fields.add(tfMonths);
+        fields.add(tfSalary);
+
+        btnReward.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int load = Integer.parseInt(tfLoad.getText());
+                    int i = 1, flag = 0;
+                    for (Person p : persons) {
+                        if (i == load) {
+                            tfAge.setText(String.valueOf(p.getAge()));
+                            tfName.setText(p.getName());
+                            flag = 1;
+                            if (p instanceof Clerk) {
+                                rbClerk.setSelected(true);
+                            } else if (p instanceof Customer) {
+                                rbCustomer.setSelected(true);
+                            } else if (p instanceof Manager) {
+                                rbManager.setSelected(true);
+                            }
+                        }
+                        i++;
+                    }
+
+                    if (flag == 0)
+                        throw new outOfReach("OutOFReach");
+                } catch (NumberFormatException x) {
+                    JOptionPane.showMessageDialog(null, String.format("Age must be an Integer"), "Message", 1);
+                } catch (outOfReach x) {
+                    JOptionPane.showMessageDialog(null, String.format("OutOfReach"), "Message", 1);
+                }
+                JOptionPane.showMessageDialog(null, String.format("Age must be an Integer"), "Message", 1);
+            }
+        });
+        btnLoad.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int load = Integer.parseInt(tfLoad.getText());
+                    int i = 1, flag = 0;
+                    for (Person p : persons) {
+                        if (i == load) {
+                            tfAge.setText(String.valueOf(p.getAge()));
+                            tfName.setText(p.getName());
+                            flag = 1;
+                            if (p instanceof Clerk) {
+                                rbClerk.setSelected(true);
+                            } else if (p instanceof Customer) {
+                                rbCustomer.setSelected(true);
+                            } else if (p instanceof Manager) {
+                                rbManager.setSelected(true);
+                            }
+                        }
+                        i++;
+                    }
+
+                    if (flag == 0)
+                        throw new outOfReach("OutOFReach");
+                } catch (NumberFormatException x) {
+                    JOptionPane.showMessageDialog(null, String.format("Age must be an Integer"), "Message", 1);
+                } catch (outOfReach x) {
+                    JOptionPane.showMessageDialog(null, String.format("OutOfReach"), "Message", 1);
+                }
+            }
+        });
+        btnSayHi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (Person p : persons) {
+                    System.out.println(p);
+                }
+            }
+        });
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (JTextField tf : fields) {
+                    tf.setText("");
+                }
+            }
+        });
+
+        btnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int num = 0;
+                for (JRadioButton rb : info) {
+                    try {
+                        if (rb.isSelected()) {
+                            String infos = rb.getText();
+                            switch (infos) {
+                                case "Clerk":
+                                    num = 1;
+                                    Clerk clerk = new Clerk();
+                                    clerk.setName(tfName.getText());
+                                    clerk.setAge(Integer.parseInt(tfAge.getText()));
+                                    String toBe = String.format("Clerk - %s (%d)\n", clerk.getName(), clerk.getAge());
+                                    append(taPersons, toBe);
+                                    persons.add(clerk);
+                                    break;
+
+                                case "Customer":
+                                    num = 2;
+                                    Customer cust = new Customer();
+                                    cust.setName(tfName.getText());
+                                    cust.setAge(Integer.parseInt(tfAge.getText()));
+                                    String custprint = String.format("Customer - %s (%d)\n", cust.getName(), cust.getAge());
+                                    append(taPersons, custprint);
+                                    persons.add(cust);
+                                    break;
+
+                                case "Manager":
+                                    num = 3;
+                                    Manager man = new Manager();
+                                    man.setName(tfName.getText());
+                                    man.setAge(Integer.parseInt(tfAge.getText()));
+                                    String manprint = String.format("Manager - %s (%d)\n", man.getName(), man.getAge());
+                                    append(taPersons, manprint);
+                                    persons.add(man);
+                                    break;
+                            }
+                        }
+                    } catch (NumberFormatException x) {
+                        JOptionPane.showMessageDialog(null, String.format("Age must be an Integer"), "Message", 1);
+                    }
+
+                }
+
+                for (JTextField tf : fields) {
+                    tf.setText("");
+                }
+
+
+            }
+        });
     }
 
     public static void main(String[] args) {
         // add here how to make GUI visible
+        App frame = new App();
+        frame.setTitle("Simple Calculator");
+        frame.setContentPane(frame.pnlMain);
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    void append(JTextArea taPersons, String newText){
+        taPersons.setText(taPersons.getText() + newText);
     }
 
     static void giveReward(int n) {
 
+    }
+}
+
+class outOfReach extends Exception {
+    public outOfReach(String s) {
+        super(s);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,49 @@
-public class Person {
+public abstract class  Person {
     // TODO implement Person and its subclasses in other Java files
+    private String name;
+    private int age;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String s) {
+        name = s;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int s) {
+        age = s;
+    }
+    @Override
+    public String toString() {
+        return String.format("Hello, my name is %s", name);
+    }
+}
+abstract class Employee extends Person {
+    private int months_worked;
+    private double salary;
+    //salary / (12 / months_worked
+    public double thirteenthmonth() {
+        double months_work = 12/months_worked;
+        return 12 / months_work;
+    }
+}
+class Clerk extends Employee {
+    @Override
+    public String toString() {
+        return "How may I help you?";
+    }
+}
+
+
+class Customer extends Person {
+
+}
+
+class Manager extends Employee {
+
 }
